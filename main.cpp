ef5bdd33937f7ed5a0352a1da0e142ff393b858c
LuHoo <lucas.hoogduin@mac.com>
2021-06-10 16:24:03 +0200

    Cleans up project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FSaudit
+//
+//  Created by Lucas on 31/05/2021.
+//
+
+#include <iostream>
+int main()
+{
+    std::cout <<"enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2
+    << " is " << v1 + v2 << std::endl;
+    return 0;
+}
