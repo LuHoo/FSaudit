24e6dc8dd7434c3dd4b45db0472e1c372c60ed3c
LuHoo <lucas.hoogduin@mac.com>
2025-05-23 15:56:28 +0200

    Removes deprecated @doctype

--- R/FSaudit.R
+++ R/FSaudit.R
@@ -4,27 +4,25 @@
 #' functions: attribute sampling, monetary unit sampling, and classical
 #' variables sampling.
 #'
-#' @section
-#'   Attribute sampling: Attribute sampling functions include the
-#'   calculation of sample sizes for fixed attribute sampling, as well as the
-#'   calculation of upper and lower bounds.
+#' @section Attribute sampling:
+#' Attribute sampling functions include the
+#' calculation of sample sizes for fixed attribute sampling, as well as the
+#' calculation of upper and lower bounds.
 #'
-#' @section
-#'   Monetary unit sampling: Monetary unit sampling is applied by auditors
-#'   as a hypothesis test to disprove the axistence of a material misstatement.
-#'   Typical stages include the calculation of a sample size, elimination of
-#'   significant items, sample selection, evaluation of the sample, and sample
-#'   extension.
+#' @section Monetary unit sampling:
+#' Monetary unit sampling is applied by auditors
+#' as a hypothesis test to disprove the existence of a material misstatement.
+#' Typical stages include the calculation of a sample size, elimination of
+#' significant items, sample selection, evaluation of the sample, and sample
+#' extension.
 #'
-#' @section
+#' @section Classical variables sampling:
+#' Classical variables sampling includes
+#' estimation with or without auxiliary variables, where the sample may or may
+#' not be stratified. Methods that use auxiliary variables include regression
+#' estimation, difference estimation, and ratio estimation. Typical stages
+#' include stratification, calculation of the sample size, sample size
+#' allocation, sample selection, evaluation of the sample, and extension.
 #'
-#'   Classical variables sampling: Classical variables sampling includes
-#'   estimation with or without auxiliary variables, where the sample may or may
-#'   not be stratified. Methods that use auxiliary variables include regression
-#'   estimation, difference estimation, and ratio estimation. Typical stages
-#'   include stratification, calculation of the sample size, sample size
-#'   allocation, sample selection, evaluation of the sample, and extension.
-#'
-#' @docType package
-#' @name FSaudit
-NULL
+#' @keywords internal
+"_PACKAGE"
